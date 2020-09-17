ede8d2639c547799a1fb99a0b5eeaf356ccc7e94
Toru Hisai <toru@torus.jp>
2020-09-17 13:10:22 +0900

    filter empty lines out

--- static/script.js
+++ static/script.js
@@ -7,8 +7,11 @@ function submit(arg) {
     const lines = line_divs.map(el => {
         let charinput = el.querySelector('input.character-input')
         let lineinput = el.querySelector('input.line-input')
-        return {character: charinput.value, text: lineinput.value}
-    })
+        if (charinput.value.length > 0 && lineinput.value.length > 0)
+            return {character: charinput.value, text: lineinput.value}
+        else
+            return null
+    }).filter(elem => elem)
 
     const data = {
         type:  document.querySelector('#type-input').value,
