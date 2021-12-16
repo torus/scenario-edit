0b04baa3352a388b92484c5eb8cacf43d48f1ac7
Toru Hisai <toru@torus.jp>
2021-12-16 13:13:53 +0900

    cosmetic change

--- lib/playlogic/play.scm
+++ lib/playlogic/play.scm
@@ -89,7 +89,7 @@
                         #;(div (@ (class "column is-2"))
                              (span (@ (class "tag is-info"))
                                    ,type))
-                        (div (@ (class "column is-one-third"))
+                        (div (@ (class "column is-6"))
                              (h4 (@ (class "title is-4")) ,label))
                         (div (@ (class "column"))
                              (p (a (@ (href ,#"/scenarios/~|data-id|/locations/~loc"))
